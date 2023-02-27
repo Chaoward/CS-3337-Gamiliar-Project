7dd78ae32ae3e6c7c599e4b0beac20a88969dd5c
Thien Ho <113329321+tho91@users.noreply.github.com>
2023-02-27 00:17:34 +0000

    adding test.js

---
+++ cmds/test.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+//USE THIS AS A TEMPLATE
+module.exports = {
+    //NOTE: any changes in 'data' must be registered again. View main.js/loadCommands() for detail
+    data: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("testing other function")
+        .addStringOption(option => 
+            option
+                .setName("extra")
+                .setDescription("extra")
+        ),
+        
+    //main function of the command
+    async execute(interact) {
+        //reading the args of a command
+        const arg = interact.options.getString("extra");
+        if ( (arg == null ? "" : arg.toLowerCase()) === "there") {
+            interact.reply({
+                embeds: [{
+                    color: 0xFFD700,
+                    image: {
+                        url: "https://c.tenor.com/smu7cmwm4rYAAAAC/general-kenobi-kenobi.gif"
+                    }
+                }]
+            });
+            return;
+        }
+
+        await interact.reply("Hello, I'm Mr. Bot. This is my code. I eat the bug.");
+    }
+};
